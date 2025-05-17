49ffc483f40947f3b3d50913f75495a656949067
Tudose Razvan <146849110+TudoseRazvan@users.noreply.github.com>
2025-05-17 14:18:18 +0300

    Delete Subiect_Decorator_Composite directory

--- Subiect_Decorator_Composite/clase/IProdus.java
+++
@@ -1,8 +0,0 @@
-package Subiect_Decorator_Composite.clase;
-
-public interface IProdus {
-    String getDenumire();
-    int getNrBucati();
-    double getPret();
-    void afiseazaDetalii(String indent);
-}
