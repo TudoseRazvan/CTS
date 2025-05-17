41a2060c169231e36dbdbcbb2fc622b376c5050d
Tudose Razvan <146849110+TudoseRazvan@users.noreply.github.com>
2025-05-17 14:15:38 +0300

    Delete S2ClubSportiv directory

--- S2ClubSportiv/prototype/JucatorPrototype.java
+++
@@ -1,8 +0,0 @@
-package S2ClubSportiv.prototype;
-
-public interface JucatorPrototype {
-    JucatorPrototype cloneaza(); //Obligatoriu
-    void adaugaAntrenament(String antrenament);
-    void adaugaMedicamentInterzis(String medicament);
-    void afisareDetalii();
-}
